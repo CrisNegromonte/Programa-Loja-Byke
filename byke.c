e0320532d8c33dd8d5f83156c3d1ed31c164ba4d
Cristina Negromonte <cristinanegromonte08@gmail.com>
2023-08-27 17:04:23 -0300

    Correção de erros nos comandos iniciais

--- byke.c
+++ byke.c
@@ -11,9 +11,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "cliente.h"
+
 
 /////
 // Assinatura das funções
