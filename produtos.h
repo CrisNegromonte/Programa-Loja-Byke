c8b24cf0e552c9132950bc37f695c38ea025bfa4
Cristina Negromonte <cristinanegromonte08@gmail.com>
2023-09-18 19:58:50 -0300

    Corrige erros nas assinat de funções menu produtos

--- produtos.h
+++ produtos.h
@@ -1,7 +1,7 @@
 void menu_produtos(void);
-void cadastrar_produtos(void);
-void pesquisar_produtos(void);
-void alterar_produtos(void);
-void excluir_produtos(void);
+void cadastrar_produto(void);
+void pesquisar_produto(void);
+void alterar_produto(void);
+void excluir_produto(void);
 
 char tela_produtos(void);
