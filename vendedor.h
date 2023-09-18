e5b59728ef677ba6d453462f7a8681b2d990c3de
Cristina Negromonte <cristinanegromonte08@gmail.com>
2023-09-18 19:47:51 -0300

    Corrige erro de sintaxe no menu vendedores

--- vendedor.h
+++ vendedor.h
@@ -1,7 +1,7 @@
-void menu_vendedor(void);
+void menu_vendedores(void);
 void cadastrar_vendedor(void);
 void pesquisar_vendedor(void);
 void alterar_vendedor(void);
 void excluir_vendedor(void);
 
-char tela_vendedor(void);
+char tela_vendedores(void);
