f2acd792b040c9dc633f1b100704370132c44dfc
Cristina Negromonte <cristinanegromonte08@gmail.com>
2023-10-02 15:41:43 -0300

    Adiciona assinaturas das fuções do arq Clientes

--- cliente.h
+++ cliente.h
@@ -3,5 +3,15 @@ void cadastrar_cliente(void);
 void pesquisar_cliente(void);
 void alterar_cliente(void);
 void excluir_cliente(void);
+void cadastrar_cliente_f(void);
+void pesquisar_cliente_f(void);
+void alterar_cliente_f(void);
+void excluir_cliente_f(void);
 
 char tela_clientes(void);
+
+void ler_cpf (char* cpf);
+void ler_nome (char* nome);
+void ler_email (char* email);
+void ler_nasc (char* nasc);
+void ler_fone (char* fone);
