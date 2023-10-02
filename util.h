317ebee1b0879e78fc1f7c9540620892f08bc01b
Cristina Negromonte <cristinanegromonte08@gmail.com>
2023-10-02 16:00:52 -0300

    Inclui assinaturas da funções no arq util.h

---
+++ util.h
@@ -0,0 +1,15 @@
+////
+//// Assinaturas das funcoes uteis
+////
+
+void delay(int);
+void limpaTela(void);
+int ehDigito(char);
+int ehLetra(char);
+int ehBissexto(int);
+int ehValido(char);
+int validarData(int, int, int);
+int validarCpf(char*);
+int validarNome(char*);
+int validarEmail(char*);
+int validarFone(char*);
