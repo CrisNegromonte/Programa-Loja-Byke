ad5af1058e4b790a02fb99a1362df06009acea06
Cristina Negromonte <cristinanegromonte08@gmail.com>
2023-10-07 20:45:24 -0300

    Adiciona assinaturas de funções em vendedor.h

--- vendedor.h
+++ vendedor.h
@@ -5,3 +5,14 @@ void alterar_vendedor(void);
 void excluir_vendedor(void);
 
 char tela_vendedores(void);
+
+void cadastrar_vendedor_f(void);
+void pesquisar_vendedor_f(void);
+void alterar_vendedor_f(void);
+void excluir_vendedor_f(void);
+
+void ler_cpf (char* cpf);
+void ler_nome (char* nome);
+void ler_email (char* email);
+void ler_nasc (char* nasc);
+void ler_fone (char* fone);
